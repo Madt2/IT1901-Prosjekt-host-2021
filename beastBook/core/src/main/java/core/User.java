74d564985a77ea533b3d4d1e17aa547dc4bb7665
tormadsen <tormads@ntnu.no>
2021-09-22 13:04:51 +0200

    Added draft for registrer user and added logic to user.java.

--- beastBook/core/src/main/java/core/User.java
+++ beastBook/core/src/main/java/core/User.java
@@ -1,4 +1,35 @@
 package core;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class User {
+
+    private String userName;
+    private String password;
+    private List<String> myWorkouts = new ArrayList<String>();
+    private List<String> myHistory = new ArrayList<String>();
+
+    public User(String userName, String password) {
+        setUserName(userName);
+        setPassword(password);
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
 }
--- beastBook/fxui/src/main/java/fxui/LoginController.java
+++ beastBook/fxui/src/main/java/fxui/LoginController.java
@@ -4,11 +4,14 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+
+import java.awt.event.ActionEvent;
 import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 
 public class LoginController {
+    private core.User user;
 
     @FXML
     private AnchorPane rootPane;
@@ -30,5 +33,19 @@ public class LoginController {
         AnchorPane pane =  FXMLLoader.load(getClass().getResource("HomeScreen.fxml"));
         rootPane.getChildren().setAll(pane);
     }
+
+    @FXML
+    void registerUser(ActionEvent event) throws IllegalArgumentException {
+        String userName = username_input.getText();
+        String password = password_input.getText();
+        user.User(userName, password);
+    }
+    /* public static void main(String[] args) {
+        LoginController controller = new LoginController();
+        controller.username_input.setText("Tor");
+        controller.password_input.setText("bruh");
+
+        controller.registerUser(ActionEvent event);
+    } */
 
 }
