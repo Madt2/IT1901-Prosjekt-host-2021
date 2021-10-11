d6ca230b963e47f645cbc07951dc8ecf7939b0c1
mklevs99 <matsi99@live.com>
2021-10-11 20:37:35 +0200

    WorkoutControllerTest added test for edit cell
    Not finished. Needs to add more test methods aswell

--- beastBook/fxui/src/test/java/fxui/WorkoutControllerTest.java
+++ beastBook/fxui/src/test/java/fxui/WorkoutControllerTest.java
@@ -5,105 +5,82 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.base.NodeMatchers;
+import org.testfx.matcher.control.LabeledMatchers;
+import org.testfx.matcher.control.TableViewMatchers;
+import org.testfx.matcher.control.TextMatchers;
+
 import core.Exercise;
 import core.Workout;
 import core.User;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.TableView;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
+import json.BeastBookPersistence;
 
 public class WorkoutControllerTest extends ApplicationTest{
     
     private WorkoutController wc;
     private User user;
 
-    @FXML
-    private TableView<Exercise> workout_table;
-
-    private Exercise e1;
-    private Exercise e2;
-    private Exercise e3;
-    private Exercise e4;
-    //private Workout w1;
-
     @Override
     public void start(final Stage stage) throws IOException {
         wc = new WorkoutController();
         final FXMLLoader loader = new FXMLLoader(this.getClass().getResource("Workout.fxml"));
         loader.setController(wc);
-        final Parent root = loader.load();
-        user = new User("Mats", "Brage");
+        user = new User();
+        user.setUserName("test");
+        addWorkoutsToUser();
         wc.setUser(user);
+        final Parent root = loader.load();
         stage.setScene(new Scene(root));
         stage.show();
     }
-
-    @BeforeEach
-    void setup(){
-        e1 = new Exercise("Deadlift", 8, 100, 3, 180);
-        e2 = new Exercise("Reverse flyes", 12, 20, 3, 120);
-        e3 = new Exercise("Wide rows", 12, 20, 3, 120);
-        e4 = new Exercise("Dumbell shrugs", 12, 30, 3, 120);
-
-        //w1 = new Workout("Back");
-        workout_table = wc.getWorkoutTable();
-        wc.getWorkout().addExercise(e1);
-        wc.getWorkout().addExercise(e2);
-        wc.getWorkout().addExercise(e3);
-        wc.getWorkout().addExercise(e4);
-        //w1.addExercise(e1);
-    }
-
+   
     @Test
-    void testSetupTableView() {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run(){
-                wc.setTable();
-                
-                // Workout name, row 0
-                System.out.println(wc.getTable(0));
-                Assertions.assertEquals("Deadlift", wc.getTable(0).getExerciseName());
-                wc.getTable(0).setExerciseName("Dumbell deadlift");
-                Assertions.assertNotEquals("Deadlift", wc.getTable(0).getExerciseName());
-                Assertions.assertEquals("Dumbell deadlift", wc.getTable(0).getExerciseName());
-                
-                // Rep goal, row 0
-                Assertions.assertEquals(8, wc.getTable(0).getRepGoal());
-                wc.getTable(0).setRepGoal(5);
-                Assertions.assertNotEquals(8, wc.getTable(0).getRepGoal());
-                Assertions.assertEquals(5, wc.getTable(0).getRepGoal());
+    void testEditSelectedCell() throws IOException{
+        Assertions.assertEquals("Benchpress", user.getWorkout("Pull workout").getExercises().get(0).getExerciseName());
+        wc.getWorkoutTable().getColumns().get(0).setId("exerciseName");
+        Node node = lookup("#exerciseName").nth(1).query();
+        doubleClickOn(node, MouseButton.PRIMARY).write("Biceps curl");
+        press(KeyCode.ENTER).release(KeyCode.ENTER);
+        //saveUserState();
         
-                /*
-                // Weight, row 0
-                Assertions.assertEquals(70, wc.getTable(0).getWeight());
-                wc.getTable(0).setWeight(100);
-                Assertions.assertNotEquals(70, wc.getTable(0).getWeight());
-                Assertions.assertEquals(100, wc.getTable(0).getWeight());
-        
-                // Time to check row 1 (second row)
-                
-                
-                // Sets, row 1
-                Assertions.assertEquals(3, wc.getTable(1).getSets());
-                wc.getTable(1).setSets(5);
-                Assertions.assertNotEquals(3, wc.getTable(1).getSets());
-                Assertions.assertEquals(5, wc.getTable(1).getSets());
-        
-                // Rest time, row 1
-                Assertions.assertEquals(60, wc.getTable(1).getRestTime());
-                wc.getTable(1).setRestTime(120);
-                Assertions.assertNotEquals(60, wc.getTable(1).getRestTime());
-                Assertions.assertEquals(120, wc.getTable(1).getRestTime());
-                */
-            }
-        });
+        //FxAssert.verifyThat("#exerciseName", LabeledMatchers.hasText("Hei"));
+        Assertions.assertEquals("Biceps curl", user.getWorkout("Pull workout").getExercises().get(0).getExerciseName());
+
+    
     }
+
+    
+
+    private void addWorkoutsToUser(){
+        Workout workout1 = new Workout("Pull workout");
+        workout1.addExercise(new Exercise("Benchpress", 20, 30, 40, 50));
+        workout1.addExercise(new Exercise("Leg press", 25, 50, 75, 100));
+        workout1.addExercise(new Exercise("Deadlift", 20, 20, 20, 20));
+        workout1.addExercise(new Exercise("Biceps curl", 20, 20, 20, 20));
+        user.addWorkout(workout1);
+
+        wc.setWorkout(workout1);
+    }
+    
+    private void saveUserState() throws IOException {
+        BeastBookPersistence persistence = new BeastBookPersistence();
+        persistence.setSaveFilePath(user.getUserName());
+        persistence.saveUser(user);
+    }
+
 }
