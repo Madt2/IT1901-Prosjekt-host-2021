f6249a48eb51ec015d45b793941649f93dbf2aba
mklevs99 <matsi99@live.com>
2021-10-26 20:06:39 +0200

    Edit CrWoControllerTest to match new functionality Now tests for delete exercise and create workout with same name

--- beastBook/fxui/src/test/java/beastbook/fxui/CreateWorkoutControllerTest.java
+++ beastBook/fxui/src/test/java/beastbook/fxui/CreateWorkoutControllerTest.java
@@ -4,12 +4,14 @@ import beastbook.core.Exercise;
 import beastbook.core.User;
 import beastbook.core.Workout;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseButton;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
@@ -21,6 +23,9 @@ import java.io.IOException;
 public class CreateWorkoutControllerTest extends ApplicationTest{
   private CreateWorkoutController controller;
   private User user;
+  private Workout workout = new Workout();
+  private Exercise exercise1 = new Exercise();
+  private Exercise exercise2 = new Exercise();
     
   @Override
   public void start(final Stage stage) throws IOException {
@@ -34,6 +39,17 @@ public class CreateWorkoutControllerTest extends ApplicationTest{
     stage.setScene(new Scene(root));
     stage.show();
   }  
+
+  @BeforeEach
+  void setup(){
+    user.getWorkouts().clear();
+    workout = new Workout("testWorkout");
+    exercise1 = new Exercise("Benchpress", 10, 20, 30, 40);
+    exercise2 = new Exercise("Squat", 40, 30, 20, 10);
+    workout.addExercise(exercise1);
+    workout.addExercise(exercise2);
+    user.addWorkout(workout);
+  }
       
   @Test
   void testInputFieldsAddsToWorkoutObject() {        
@@ -114,7 +130,9 @@ public class CreateWorkoutControllerTest extends ApplicationTest{
   }
 
   @Test
-  void canNotSaveWorkoutWithoutName(){
+  void testCanNotSaveWorkoutWithoutName(){
+    user.removeWorkout(workout);
+
     clickOn("#exerciseNameInput", MouseButton.PRIMARY).write("Squat");
     clickOn("#repGoalInput", MouseButton.PRIMARY).write("50");
     clickOn("#weigthInput", MouseButton.PRIMARY).write("20");
@@ -126,12 +144,26 @@ public class CreateWorkoutControllerTest extends ApplicationTest{
     FxAssert.verifyThat("#exceptionFeedback", TextMatchers.hasText("Input title is empty, please enter name to workout"));
     Assertions.assertEquals(0, user.getWorkouts().size());
   }
+
+  @Test
+  void testCanNotCreateWorkoutWithSameName(){
+    clickOn("#titleInput", MouseButton.PRIMARY).write(workout.getName());
+    clickOn("#exerciseNameInput", MouseButton.PRIMARY).write("B");
+    clickOn("#repGoalInput", MouseButton.PRIMARY).write("1");
+    clickOn("#weigthInput", MouseButton.PRIMARY).write("2");
+    clickOn("#setsInput", MouseButton.PRIMARY).write("3");
+    clickOn("#restInput", MouseButton.PRIMARY).write("4");
+    clickOn("#addButton", MouseButton.PRIMARY);
+    clickOn("#createButton", MouseButton.PRIMARY);
+    FxAssert.verifyThat("#exceptionFeedback", TextMatchers.hasText("User already has workout testWorkout saved! Workout was not created, please choose another name."));
+    Assertions.assertEquals("Benchpress", user.getWorkout("testWorkout").getExercises().get(0).getExerciseName());
+    Assertions.assertNotEquals("Squat", user.getWorkout("testWorkout").getExercises().get(0).getExerciseName());
+  }
     
   @Test
-  void workoutIsNotLoaded(){
-    Workout workout = new Workout("testWorkout");
-    workout.addExercise(new Exercise("Benchpress", 20, 20, 20, 20));
-     
+  void testWorkoutIsNotLoaded(){   
+    user.removeWorkout(workout);  
+
     clickOn("#titleInput", MouseButton.PRIMARY).write("testWorkout");
     clickOn("#loadButton", MouseButton.PRIMARY);
 
@@ -140,20 +172,41 @@ public class CreateWorkoutControllerTest extends ApplicationTest{
   }
 
   @Test
-  void workoutIsLoaded(){
-    Workout workout = new Workout("testWorkout");
-    workout.addExercise(new Exercise("Benchpress", 20, 30, 40, 50));
-    user.addWorkout(workout);
-
+  void testWorkoutIsLoaded(){
     clickOn("#titleInput", MouseButton.PRIMARY).write("testWorkout");
     clickOn("#loadButton", MouseButton.PRIMARY);
 
-    Assertions.assertEquals("Benchpress", controller.getTable(0).getExerciseName());
-    Assertions.assertEquals(20, controller.getTable(0).getRepGoal());
-    Assertions.assertEquals(30, controller.getTable(0).getWeight());
-    Assertions.assertEquals(40, controller.getTable(0).getSets());
-    Assertions.assertEquals(50, controller.getTable(0).getRestTime());
+    controller.getWorkoutTable().getColumns().get(0).setId("exerciseName");
+    Node node = lookup("#exerciseName").nth(1).query();
+    clickOn(node);
+
+    Exercise loadedExercise = controller.getWorkoutTable().getSelectionModel().getSelectedItem();
+    Assertions.assertEquals(exercise1, loadedExercise);
   }
+
+  @Test
+  void testDeleteExercise() throws InterruptedException{
+    clickOn("#titleInput", MouseButton.PRIMARY).write("testWorkout");
+    clickOn("#loadButton", MouseButton.PRIMARY);
+
+    controller.getWorkoutTable().getColumns().get(0).setId("exerciseName");
+    Node node = lookup("#exerciseName").nth(1).query();
+    clickOn(node);
+    clickOn("#deleteButton");
+
+    controller.getWorkoutTable().getColumns().get(0).setId("exerciseName");
+    Assertions.assertEquals(1, user.getWorkout("testWorkout").getExercises().size());
+    Node node2 = lookup("#exerciseName").nth(1).query();
+    clickOn(node2);
+
+    Exercise lastExercise = controller.getWorkoutTable().getSelectionModel().getSelectedItem();
+    Assertions.assertEquals(exercise2, lastExercise);
+
+    clickOn("#deleteButton");
+    FxAssert.verifyThat("#exceptionFeedback", TextMatchers.hasText("Could not delete exercise '" + lastExercise.getExerciseName() 
+    + "', at least one exercise has to be in every workout!"));
+  }
+ 
 
   @AfterAll
   static void cleanUp() {
