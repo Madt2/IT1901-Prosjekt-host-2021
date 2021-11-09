cd86aaf9c3015c4503422803ccb220f71d1a2690
tormadsen <tormads@ntnu.no>
2021-11-09 18:30:15 +0100

    Added rest module. minor change to module-info.java in core.

--- beastBook/core/src/main/java/module-info.java
+++ beastBook/core/src/main/java/module-info.java
@@ -2,5 +2,4 @@ module beastbook.core {
     requires com.fasterxml.jackson.core;
     requires transitive com.fasterxml.jackson.databind;
     exports beastbook.core;
-    exports beastbook.json;
 }
